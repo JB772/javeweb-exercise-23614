5b742135c92a667a7ad3d8c814ae45d3f1ce68bf
洪展彬 <jambing78@gmail.com>
2020-08-15 06:37:07 +0800

    webContent

--- WebContent/member/js/login.js
+++ WebContent/member/js/login.js
@@ -1,24 +1,37 @@
 function de() {
-    var n = document.getElementById("name1");
-    var name1 = n.value;
+    var Account = document.getElementById("Account");
+    var account = Account.value;
     var reg = new RegExp(/^\w{6,20}$/);
-    if (n != null && reg.test(name1)) {
+    if (n != null && reg.test(account)) {
         alert("姓名正確");
     } else {
         alert("重新輸入");
     }
-    var n = document.getElementById("pwd");
-    var pass = n.value;
+    var pwd = document.getElementById("pwd");
+    var password = pwd.value;
     var reg1 = new RegExp(/^[a-zA-Z0-9_-]{6,16}$/);
-    if (n != null && reg1.test(pass)) {
+    if (n != null && reg1.test(password)) {
         alert("密碼正確");
     } else {
         alert("密碼錯誤");
     }
-    var n = document.getElementById("nickname");
-    var nickname = n.value;
+    var nickName = document.getElementById("nickname");
+    var nickname = nickName.value;
     var reg2 = new RegExp(/^\w{5,20}$/);
-    if (name1 == reg == reg2 && reg1 == pass && reg2 == nickname) {
+    if (account == reg == reg2 && reg1 == password && reg2 == nickname) {
         alert("登陸成功");
     }
+    const obj = {account: acount, password: password};
+    
+    fetch("./UseJsonServlet", {
+    	
+    	method: "POST",
+    	headers: { "Content-Type": "application/json" },
+    	body: JSON.stringify(obj)
+    })
+    	.then(resp => resp.json())
+    	.then(body => {
+    		// 接收到Controller回應後，要做的事寫在這!!
+    		alert(body.lastUpdateDate);
+    	});
 }
--- src/web/member/dao/impl/MemberDaoimpl.java
+++ src/web/member/dao/impl/MemberDaoimpl.java
@@ -4,12 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.List;
+
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
-
-import com.mysql.cj.protocol.Resultset;
-import com.zaxxer.hikari.HikariDataSource;
 
 import web.member.bean.Member;
 import web.member.dao.MemberDao;
